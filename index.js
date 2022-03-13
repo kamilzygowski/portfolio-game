918fa30bceafb19618b85bf65b31211a65536a38
Kamil Zygowski <kzygowski7@gmail.com>
2022-03-13 14:43:23 +0100

    typescript added, basic concept of project

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const player = document.querySelector('.player');
-console.log(player);
-console.log('xd')
+console.log(player.style.left);
+console.log('xd');
+document.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    let playerLeftPos = '';
+    console.log(Array.from(player.style.left));
+    for (let i = 0; i < Array.from(player.style.left).length - 2; i++) {
+        playerLeftPos += Array.from(player.style.left)[i];
+    }
+    console.log(playerLeftPos);
+    switch (e.key) {
+        case 'd': {
+            player.style.left = `${parseInt(playerLeftPos) + 50}px`;
+            console.log('it fired');
+            break;
+        }
+        case 'a': {
+            player.style.left = `${parseInt(playerLeftPos) - 50}px`;
+            console.log('it fired');
+            break;
+        }
+    }
+});
---
+++ index.ts
@@ -0,0 +1,27 @@
+const player:any = document.querySelector('.player');
+console.log(player.style.left);
+console.log('xd')
+
+document.addEventListener('keydown', (e)=>{
+console.log(e.key)
+//const playerLeftPos = Array.from(player.style.left).splice(0, player.style.left.length - 2).join("")
+let playerLeftPos ='';
+console.log(Array.from(player.style.left))
+for (let i =0; i < Array.from(player.style.left).length-2; i++){
+    
+    playerLeftPos += Array.from(player.style.left)[i]
+}
+
+console.log(playerLeftPos)
+
+switch(e.key){
+    case 'd': {player.style.left = `${parseInt(playerLeftPos) + 50}px`;
+    console.log('it fired');
+    break;
+}
+case 'a': {player.style.left = `${parseInt(playerLeftPos) - 50}px`;
+    console.log('it fired');
+    break;
+}
+}
+})
